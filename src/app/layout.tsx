54e0bffc736ec07e66a33c5049ca835676cf5156
Gildasio Anjos <gildasioverasdossnjos@gmail.com>
2025-08-18 18:20:04 +0000

    CONVERTA ESSA PLATAFORMA COMPLETAMENTE EM PWA POR FAVOR QUEROFAZER UM TE

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import { ThemeProvider } from 'next-themes';
 export const metadata: Metadata = {
   title: 'SmartWheels',
   description: 'SmartWheels: Táxi Inteligente, conectando você globalmente.',
+  manifest: '/manifest.json',
 };
 
 export default function RootLayout({
@@ -20,6 +21,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap" rel="stylesheet" />
+        <meta name="theme-color" content="#32CD32" />
         <script
           dangerouslySetInnerHTML={{
             __html: `
