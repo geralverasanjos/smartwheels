42284933a2f3449c3ca09984014702591607ff75
Gildasio Anjos <gildasioverasdossnjos@gmail.com>
2025-08-20 16:30:39 +0000

    foi convertida em PWA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,8 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap" rel="stylesheet" />
-        <meta name="theme-color" content="#22c55e" />
+        <meta name="theme-color" content="#32CD32" />
+        <link rel="manifest" href="/manifest.json" />
         <script
           dangerouslySetInnerHTML={{
             __html: `
