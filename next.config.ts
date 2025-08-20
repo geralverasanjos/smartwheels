2e7e9c9490bdd5de8e6310d6e7bc7147770dd0ce
Gildasio Anjos <gildasioverasdossnjos@gmail.com>
2025-08-20 20:58:00 +0000

    NAO CONSIGO

--- next.config.ts
+++ next.config.ts
@@ -10,12 +10,6 @@ const withPWA = require("@ducanh2912/next-pwa").default({
 
 const nextConfig: NextConfig = {
   /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
     remotePatterns: [
       {
